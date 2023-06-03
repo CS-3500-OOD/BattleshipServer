dc4823e036aee5fd5a1ef4f25a0a2c466367e458
Nick Selvitelli <nickselvitelli@gmail.com>
2023-06-03 19:28:45 -0400

    make jar build cleaner

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 
-
-
 plugins {
     id("java")
     application
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -24,22 +22,23 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.20.0")
     // https://central.sonatype.com/artifact/org.apache.logging.log4j/log4j-core/2.20.0
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
-
 }
+
 
 tasks.test {
     useJUnitPlatform()
 }
 
+var makeServerJar : Boolean = true
+
 application {
-//    mainClass.set("client.Client")
-    mainClass.set("server.Server")
+    mainClass.set(if(makeServerJar) "server.Server" else "client.Client")
 }
 
 tasks {
     val fatJar = register<Jar>("fatJar") {
         dependsOn.addAll(listOf("compileJava", "processResources")) // We need this for Gradle optimization to work
-        archiveClassifier.set("standalone") // Naming the jar
+        archiveFileName.set(if(makeServerJar) "Server.jar" else "Client.jar")
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
         manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
         val sourcesMain = sourceSets.main.get()
